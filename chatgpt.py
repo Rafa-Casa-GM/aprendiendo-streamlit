9302c7106430c2dc3b8fddfa973af8e8e3b4df0d
Rafa-Casa-GM <122168146+Rafa-Casa-GM@users.noreply.github.com>
2024-05-29 17:38:13 +0200

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
+st.title("Esto es un clon de ChatGPT")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
